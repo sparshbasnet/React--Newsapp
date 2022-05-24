daea7eff319ffb52c5796be02239595d9b0b8a32
Sparsh Basnet <ashubasnet80@gmail.com>
2022-05-24 14:49:31 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+export default class App extends Component {
+  render() {
+    return(
+     <div>
+       <Router>
+      <Navbar/>
+      <Routes>
+      <Route path="/" element={<News pageSize={11} country="us" category="General"/>}></Route>
+      <Route path="/Business"element={<News pageSize={11} country="us" category="Business"/>}></Route>
+      <Route path="/Entertainment"element={<News pageSize={11} country="us" category="Entertainment"/>}></Route>
+      <Route path="/Health"element={<News pageSize={11} country="us" category="Health"/>}></Route>
+      <Route path="/Science"element={<News pageSize={11} country="us" category="Science"/>}></Route>
+      <Route path="/Sports"element={<News pageSize={11} country="us" category="Sports"/>}></Route>
+      <Route path="/Technology"element={<News pageSize={11} country="us" category="Technology"/>}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
---
+++ src/components/News.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import NewsItem from "../NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
+
+
+export default class News extends Component {
+  static defaultProps = {
+    country: 'us',
+    pageSize:'8',
+    category:'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
+  constructor() {
+    super();
+    //console.log("Hello I am a constructor from news component")
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6be4ed6ebe184960ab17d1d9f4817d1e&page=1&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
+      let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles, totalResults:parsedData.totalResults,loading:false });
+  }
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6be4ed6ebe184960ab17d1d9f4817d1e&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading:false
+    });
+  };
+  handleNextClick = async ()=>{
+    if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6be4ed6ebe184960ab17d1d9f4817d1e&page=${this.state.page +1 }&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      loading: false
+    })
+  }
+  }
+  render() {
+    return (
+      <div>
+        <div className="container my-3">
+          <h1 className="text-center" style={{margin:'30px'}}>NewsMonkey -Top headlines</h1>
+          {this.state.loading && <Spinner/>}
+          <div className="row">
+            {!this.state.loading && this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 30) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 80)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="container d-flex justify-content-between">
+            <button
+              disabled={this.state.page <= 1}
+              type="button"
+              className="btn btn-dark"
+              onClick={this.handlePreviousClick}
+            >
+              &larr;Previous
+            </button>
+            <button
+              type="button"
+              disabled={this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)}
+              className="btn btn-dark"
+              onClick={this.handleNextClick}
+            >
+              Next &rarr;
+            </button>
+          </div>
+      </div>
+    );
+  }
+}
